e275700c5a829d13aaabdc71e4eb68b42c95b7cd
Casian Zbona <casian@floatingdots.co>
2020-07-21 17:38:47 +0300

    added chimpstatic script

--- components/Header.js
+++ components/Header.js
@@ -4,13 +4,13 @@ import Head from "next/head";
 import { Image } from "grommet";
 
 export default class Header extends Component {
-    render() {
-        return (
-            <Fragment>
-                <Head>
-                    <style
-                        dangerouslySetInnerHTML={{
-                            __html: `
+  render() {
+    return (
+      <Fragment>
+        <Head>
+          <style
+            dangerouslySetInnerHTML={{
+              __html: `
                     @font-face {
                         font-family: 'BasisGrotesquePro';
                         src: url('/static/fonts/BasisGrotesquePro-Regular.otf') format('opentype');
@@ -33,33 +33,42 @@ export default class Header extends Component {
                         src: url('/static/fonts/BasisGrotesquePro-Bold.otf') format('opentype');
                     }
                 `,
-                        }}
-                    />
+            }}
+          />
 
-                    <title>{this.props.pageTitle}</title>
-                    <meta
-                        name="description"
-                        content="The Raven’s Nest is the perfect mountain hideaway designed for outdoor enthusiasts looking for unique accommodation. Plan your trip to Transylvania now!"
-                    />
-                    <meta charSet="utf-8" />
-                    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-                    <link rel="shortcut icon" type="image/x-icon" href="/static/favicon.ico" />
-                    <link rel="stylesheet" type="text/css" href="/static/style.css" />
-                    <link rel="stylesheet" type="text/css" href="/static/aos.css" />
-                    <script
-                        async
-                        src="https://www.googletagmanager.com/gtag/js?id=UA-78808747-1"></script>
-                    <script
-                        dangerouslySetInnerHTML={{
-                            __html: `window.dataLayer = window.dataLayer || [];
+          <title>{this.props.pageTitle}</title>
+          <meta
+            name="description"
+            content="The Raven’s Nest is the perfect mountain hideaway designed for outdoor enthusiasts looking for unique accommodation. Plan your trip to Transylvania now!"
+          />
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <link
+            rel="shortcut icon"
+            type="image/x-icon"
+            href="/static/favicon.ico"
+          />
+          <link rel="stylesheet" type="text/css" href="/static/style.css" />
+          <link rel="stylesheet" type="text/css" href="/static/aos.css" />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-78808747-1"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
 					 function gtag(){dataLayer.push(arguments);}
 					 gtag('js', new Date());
 					 gtag('config', 'UA-78808747-1');`,
-                        }}></script>
+            }}
+          ></script>
 
-                    <script
-                        dangerouslySetInnerHTML={{
-                            __html: `
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
 					(function(h,o,t,j,a,r){
 					   h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
 					   h._hjSettings={hjid:255130,hjsv:6};
@@ -68,21 +77,38 @@ export default class Header extends Component {
 					   r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
 					   a.appendChild(r);
 					   })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
-                        }}></script>
-                    <script
-                        type="text/javascript"
-                        id="cookieinfo"
-                        src="//cookieinfoscript.com/js/cookieinfo.min.js"
-                        data-bg="#7c5e49"
-                        data-fg="#FFFFFF"
-                        data-link="#F1D600"
-                        data-moreinfo="/policies"
-                        data-message='This site uses cookies. By continuing to use this site, closing this banner, or clicking "I Agree", you agree to the use of cookies. Read our cookies policy for more information.If you decline, your information won’t be tracked when you visit this website. A single cookie will be used in your browser to remember your preference not to be tracked.'
-                        data-cookie="CookieInfoScript"
-                        data-text-align="left"
-                        data-close-text="I Agree"></script>
-                </Head>
-            </Fragment>
-        );
-    }
+            }}
+          ></script>
+          <script
+            id="mcjs"
+            dangerouslySetInnerHTML={{
+              __html: `
+            !function(c,h,i,m,p)
+            {
+              ((m = c.createElement(h)),
+              (p = c.getElementsByTagName(h)[0]),
+              (m.async = 1),
+              (m.src = i),
+              p.parentNode.insertBefore(m, p))
+            }
+            (document,"script","https://chimpstatic.com/mcjs-connected/js/users/a8065813dd66315c82453ee67/3bf13728e7caca98fd6460614.js");`,
+            }}
+          ></script>
+          <script
+            type="text/javascript"
+            id="cookieinfo"
+            src="//cookieinfoscript.com/js/cookieinfo.min.js"
+            data-bg="#7c5e49"
+            data-fg="#FFFFFF"
+            data-link="#F1D600"
+            data-moreinfo="/policies"
+            data-message='This site uses cookies. By continuing to use this site, closing this banner, or clicking "I Agree", you agree to the use of cookies. Read our cookies policy for more information.If you decline, your information won’t be tracked when you visit this website. A single cookie will be used in your browser to remember your preference not to be tracked.'
+            data-cookie="CookieInfoScript"
+            data-text-align="left"
+            data-close-text="I Agree"
+          ></script>
+        </Head>
+      </Fragment>
+    );
+  }
 }
